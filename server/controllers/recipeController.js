1afe379d2f9068e13994047cf955e0de23f4e9e0
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2023-04-09 20:26:42 +0530

    Setup MongoDB and schema

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,67 @@
+require("../models/database");
+const Category = require("../models/Category");
+const Recipe = require("../models/Recipe");
+
 exports.homepage = async (req, res) => {
-  res.render("index", { title: "Cooking Blog - Home" });
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render("index", { title: "Cooking Blog - Home", categories });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occure" });
+  }
 };
+
+// Get categories /categories
+
+exports.exploreCategories = async (req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render("categories", {
+      title: "Cooking Blog - Categories",
+      categories,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occure" });
+  }
+};
+
+// -------------------------------Example of inserting data in db
+
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "Thai",
+//         image: "thai-food.jpg",
+//       },
+//       {
+//         name: "American",
+//         image: "american-food.jpg",
+//       },
+//       {
+//         name: "Chinese",
+//         image: "chinese-food.jpg",
+//       },
+//       {
+//         name: "Mexican",
+//         image: "mexican-food.jpg",
+//       },
+//       {
+//         name: "Indian",
+//         image: "indian-food.jpg",
+//       },
+//       {
+//         name: "Spanish",
+//         image: "spanish-food.jpg",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log("err", +error);
+//   }
+// }
+
+// insertDummyCategoryData();
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
 router.get("/", recipeController.homepage);
+router.get("/categories", recipeController.exploreCategories);
 
 module.exports = router;
