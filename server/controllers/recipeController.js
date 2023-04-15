6d44792153afb8b26b4d20c4920cb6784c571641
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2023-04-15 15:20:46 +0530

    Submit recipe handled

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -98,7 +98,10 @@ exports.exploreLatest = async (req, res) => {
     const limitNumber = 20;
     const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
 
-    res.render("explore-latest", { title: "Cooking Blog - Explore Latest", recipe });
+    res.render("explore-latest", {
+      title: "Cooking Blog - Explore Latest",
+      recipe,
+    });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occured" });
   }
@@ -107,18 +110,76 @@ exports.exploreLatest = async (req, res) => {
 //explore random---------------------
 exports.exploreRandom = async (req, res) => {
   try {
-   
     let count = await Recipe.find().countDocuments();
-    let random = Math.floor(Math.random()*count);
+    let random = Math.floor(Math.random() * count);
     let recipe = await Recipe.findOne().skip(random).exec();
 
-    res.render("explore-random", { title: "Cooking Blog - Explore Latest", recipe });
+    res.render("explore-random", {
+      title: "Cooking Blog - Explore Latest",
+      recipe,
+    });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error Occured" });
   }
 };
 
+//submit recipe---------------
 
+exports.submitRecipe = async (req, res) => {
+  try {
+    const infoErrorsObj = req.flash("infoErrors");
+    const infoSubmitObj = req.flash("infoSubmit");
+
+    res.render("submit-recipe", {
+      title: "Cooking Blog - Submit Recipe",
+      infoErrorsObj,
+      infoSubmitObj,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
+  }
+};
+
+//POST  ------------- submit recipe---------------
+
+exports.submitRecipeOnPost = async (req, res) => {
+  try {
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      console.log("No Files where uploaded.");
+    } else {
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath =
+        require("path").resolve("./") + "/public/uploads/" + newImageName;
+
+      imageUploadFile.mv(uploadPath, function (err) {
+        if (err) return res.satus(500).send(err);
+      });
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName,
+    });
+
+    await newRecipe.save();
+
+    req.flash("infoSubmit", "Recipe has been added.");
+    res.redirect("/submit-recipe");
+  } catch (error) {
+    req.flash("infoErrorsObj", error);
+    res.redirect("/submit-recipe");
+  }
+};
 
 // -------------------------------Example of inserting data in db
 
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -9,5 +9,7 @@ router.get("/categories/:id", recipeController.exploreCategoriesById);
 router.post("/search", recipeController.searchRecipe);
 router.get("/explore-latest", recipeController.exploreLatest);
 router.get("/explore-random", recipeController.exploreRandom);
+router.get("/submit-recipe", recipeController.submitRecipe);
+router.post("/submit-recipe", recipeController.submitRecipeOnPost);
 
 module.exports = router;   
